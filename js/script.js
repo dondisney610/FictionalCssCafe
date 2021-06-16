61aca844c78ca96873b8bfb6884b17e58e221657
dondisney610 <84581072+dondisney610@users.noreply.github.com>
2021-06-16 20:25:39 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,26 @@ $(function(){
     $(this).toggleClass('-active');
     $('.header-nav').toggleClass('-active');
   });
+  
+    $('#header-nav-Homeli').on('click',function(){
+    $('.header-nav').toggleClass('-active');
+    $('#nav-btn').toggleClass('-active');
+  });
+
+  $('#header-nav-newsli').on('click',function(){
+    $('.header-nav').toggleClass('-active');
+    $('#nav-btn').toggleClass('-active');
+  });
+
+  $('#header-nav-accessli').on('click',function(){
+    $('.header-nav').toggleClass('-active');
+    $('#nav-btn').toggleClass('-active');
+  });
+
+  $('#header-nav-contactli').on('click',function(){
+    $('.header-nav').toggleClass('-active');
+    $('#nav-btn').toggleClass('-active');
+  });
 
 
   AOS.init();
